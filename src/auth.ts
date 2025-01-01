77882b1e734988634ddf6a4577a5ce814489bb6e
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-01 16:04:01 -0500

    setting up github provider for oAuth login

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,14 @@
 import NextAuth from 'next-auth';
 import prisma from './db';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import GitHub from 'next-auth/providers/github';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
    adapter: PrismaAdapter(prisma),
-   providers: [],
+   providers: [
+      GitHub({
+         clientId: process.env.GITHUB_CLIENT_ID,
+         clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      }),
+   ],
 });
