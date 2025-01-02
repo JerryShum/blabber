8447dca725f4025e57ec71c5987849e9a532d679
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-02 18:28:13 -0500

    creating a logo component to use in the navbar

---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import BlabberLogo from '/public/Blabber-logo.png';
+
+export default function Logo() {
+   return (
+      <div className="flex gap-2 items-center ">
+         <div className="mb-1">
+            <Image
+               src={BlabberLogo}
+               alt="Blabber Logo"
+               width={48}
+               height={48}
+            />
+         </div>
+
+         <h1 className="font-josefin text-3xl font-semibold bg-gradient-to-r bg-clip-text text-transparent from-zinc-800 to-zinc-950 leading-none">
+            Blabber
+         </h1>
+      </div>
+   );
+}
