8e3b05e7a8dadc05f63b2dcac94dc99668913948
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-01 21:54:43 -0500

    adding blurb model to DB schema

--- src/paths.ts
+++ src/paths.ts
@@ -2,4 +2,15 @@ const paths = {
    home() {
       return '/';
    },
+   blurbIDPage(blurbID: string) {
+      return `/blurbs/${blurbID}`;
+   },
+   blurbCreate() {
+      return `/blurbs/new`;
+   },
+   userPage(username: string) {
+      return `/user/${username}`;
+   },
 };
+
+export default paths;
