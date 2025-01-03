63e77b42a72056d3169f4f75e5b113acabcaaaf5
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-03 14:49:18 -0500

    implement buttons into the login form

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,15 +1,16 @@
+import GithubButton from '../common/GithubButton';
+import GoogleButton from '../common/GoogleButton';
 import { Button } from '../UI/button';
 import { Input } from '../UI/input';
 import { Label } from '../UI/label';
 
 export default function LoginForm() {
    return (
-      <div className="p-8  rounded-xl flex flex-col items-left justify-center max-w-96 h-full font-josefin">
-         <h2 className="text-4xl font-semibold text-left">Login</h2>
-         <div className="flex flex-col mt-4 min-h-full min-w-64">
-            <h4 className="text-center text-muted-foreground my-4">or</h4>
-
-            <div className="flex flex-col gap-4"></div>
+      <div className="font-josefin w-80">
+         <h2 className="text-4xl font-semibold text-left mb-10">Login</h2>
+         <div className="flex flex-col gap-4">
+            <GithubButton />
+            <GoogleButton />
          </div>
       </div>
    );
