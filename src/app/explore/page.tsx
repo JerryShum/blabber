9e3b5fba402f97ad8a4539fe51457055f5bf198e
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-03 13:11:51 -0500

    define about and explore pages

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,1 +1,8 @@
 //! Page for exploring all the different articles and topics
+export default function ExplorePage() {
+   return (
+      <div>
+         <h1>Explore Page</h1>
+      </div>
+   );
+}
