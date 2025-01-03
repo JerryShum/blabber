6d7e5f12dc213a01274f87d31b83547a57dffb23
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-03 13:48:56 -0500

    made logo clickable

--- src/components/nav/Logo.tsx
+++ src/components/nav/Logo.tsx
@@ -1,21 +1,24 @@
 import Image from 'next/image';
 import BlabberLogo from '/public/Blabber-logo.png';
+import Link from 'next/link';
 
 export default function Logo() {
    return (
-      <div className="flex gap-2 items-center ">
-         <div className="mb-1">
-            <Image
-               src={BlabberLogo}
-               alt="Blabber Logo"
-               width={48}
-               height={48}
-            />
-         </div>
+      <Link href={'/'}>
+         <div className="flex gap-2 items-center ">
+            <div className="mb-1">
+               <Image
+                  src={BlabberLogo}
+                  alt="Blabber Logo"
+                  width={48}
+                  height={48}
+               />
+            </div>
 
-         <h1 className="font-josefin text-3xl font-semibold bg-gradient-to-r bg-clip-text text-transparent from-zinc-800 to-zinc-950 leading-none">
-            Blabber
-         </h1>
-      </div>
+            <h1 className="font-josefin text-3xl font-semibold bg-gradient-to-r bg-clip-text text-transparent from-zinc-800 to-zinc-950 leading-none">
+               Blabber
+            </h1>
+         </div>
+      </Link>
    );
 }
