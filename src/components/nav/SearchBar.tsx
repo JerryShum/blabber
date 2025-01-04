afd05fd5f2ecb4c16bbf3468b6c10c30effdfed8
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-04 17:23:26 -0500

    adjusting the min size of the searchbar

--- src/components/nav/SearchBar.tsx
+++ src/components/nav/SearchBar.tsx
@@ -2,7 +2,7 @@ import { Input } from '../UI/input';
 
 export default function SearchBar() {
    return (
-      <div className="flex-grow max-w-72">
+      <div className="flex-grow max-w-72 min-w-48">
          <Input
             type="text"
             placeholder="Search..."
