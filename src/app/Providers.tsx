2bdd806140eb84b640dcb1be84e604e89a25b1c7
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-03 20:52:44 -0500

    adding a new theme provider

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,11 +1,20 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 interface ProviderProps {
    children: React.ReactNode;
+   themeProviderProps?: React.ComponentProps<typeof NextThemesProvider>; // Add this to pass theme props
 }
 
-export default function Providers({ children }: ProviderProps) {
-   return <SessionProvider>{children}</SessionProvider>;
+export default function Providers({
+   children,
+   themeProviderProps,
+}: ProviderProps) {
+   return (
+      <NextThemesProvider {...themeProviderProps}>
+         <SessionProvider>{children}</SessionProvider>
+      </NextThemesProvider>
+   );
 }
