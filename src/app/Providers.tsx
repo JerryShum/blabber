0b084c6a612dc4142b3c0eb4afa0be72f8cb807d
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-03 21:06:30 -0500

    making it so that the providers don't render before the client has mounted

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,18 +2,31 @@
 
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 interface ProviderProps {
    children: React.ReactNode;
-   themeProviderProps?: React.ComponentProps<typeof NextThemesProvider>; // Add this to pass theme props
 }
 
-export default function Providers({
-   children,
-   themeProviderProps,
-}: ProviderProps) {
+export default function Providers({ children }: ProviderProps) {
+   const [mounted, setMounted] = useState(false);
+
+   useEffect(() => {
+      setMounted(true); // Ensure this component only renders after the client has mounted
+   }, []);
+
+   if (!mounted) {
+      // Prevent rendering on the server to avoid mismatched themes
+      return null;
+   }
+
    return (
-      <NextThemesProvider {...themeProviderProps}>
+      <NextThemesProvider
+         attribute="class"
+         defaultTheme="system"
+         enableSystem // Synchronize with the system preference
+         disableTransitionOnChange
+      >
          <SessionProvider>{children}</SessionProvider>
       </NextThemesProvider>
    );
