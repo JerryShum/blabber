85a3f0f9372d9df752c7682246b33ed30f56870a
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-04 19:08:21 -0500

    adjusting navbar styling

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -6,7 +6,7 @@ import { DarkModeToggle } from './DarkModeToggle';
 
 export default function Navbar() {
    return (
-      <nav className="py-2 flex gap-14 items-center justify-center">
+      <nav className="py-3 flex gap-14 items-center justify-center">
          <Logo />
          <NavLinks />
          <SearchBar />
