3cf4c11e4254bf73d4e08735a39527807e7d6e12
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-05 00:10:00 -0500

    creating the display/general blurb component

---
+++ src/components/blurbs/DisplayBlurb.tsx
@@ -0,0 +1,63 @@
+import { Blurb } from '@prisma/client';
+import Image from 'next/image';
+import { ReactNode } from 'react';
+import { AspectRatio } from '../UI/aspect-ratio';
+import Author from './Author';
+import Link from 'next/link';
+import paths from '@/paths';
+
+interface DisplayBlurbProps {
+   blurb: Blurb;
+}
+
+const dummyUser: User = {
+   id: '1',
+   name: 'John Doe',
+   email: 'john.doe@example.com',
+   emailVerified: new Date(),
+   image: 'https://github.com/shadcn.png',
+};
+
+export default function DisplayBlurb({ blurb }: DisplayBlurbProps): ReactNode {
+   return (
+      <div className="rounded-xl ">
+         <Link href={paths.blurbIDPage(blurb.id)}>
+            <div className="relative">
+               <AspectRatio ratio={16 / 9}>
+                  <Image
+                     src={blurb.image}
+                     alt="Blurb image"
+                     fill
+                     quality={50}
+                     className="h-full w-full rounded-xl object-cover brightness-95 shadow-inner"
+                  />
+               </AspectRatio>
+
+               <div className="absolute bottom-0 left-0 p-4 flex flex-col items-start justify-start gap-4">
+                  <Author user={dummyUser} />
+                  <div className="flex gap-4 text-zinc-200 opacity-80 font-josefin items-center justify-end">
+                     <h4>
+                        {new Date(blurb.createdAt).toLocaleDateString('en-US', {
+                           month: 'numeric',
+                           day: 'numeric',
+                           year: 'numeric',
+                        })}
+                     </h4>
+                     <span>|</span>
+                     <h4>{blurb.estimatedRead} mins read time</h4>
+                  </div>
+               </div>
+            </div>
+
+            <div className="mt-2">
+               <h2 className=" text-xl font-noto text-foreground">
+                  {blurb.title}
+               </h2>
+               <h3 className="font-noto text-muted-foreground">
+                  {blurb.description}
+               </h3>
+            </div>
+         </Link>
+      </div>
+   );
+}
