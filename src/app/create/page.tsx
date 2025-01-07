05406fb3862d59556d1eb4d2c4ed0e5613ca68b7
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-06 21:12:08 -0500

    protecting this route (if user is not signed in)

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,4 +1,9 @@
-export default function CreatePage() {
+import { auth } from '@/auth';
+import NotAuthenticated from '@/components/common/NotAuthenticated';
+
+export default async function CreatePage() {
+   const session = await auth();
+   if (!session) return <NotAuthenticated />;
    return (
       <div>
          <h1>Create Blurb</h1>
