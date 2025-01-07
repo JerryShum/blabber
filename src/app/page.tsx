9f11eaff355233ed496a5db97fa683e4c933a89b
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-06 20:55:52 -0500

    adding datapoints (community highlights) to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import DisplayBlurb from '@/components/blurbs/DisplayBlurb';
 import HeroBlurb from '@/components/blurbs/HeroBlurb';
 
 import { Blurb } from '@prisma/client';
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
+import paths from '@/paths';
+import DataPoint from '@/components/common/DataPoint';
 
 const dummyBlurb: Blurb = {
    id: 1,
@@ -19,12 +23,53 @@ export default function Home() {
    return (
       <div className="">
          <HeroBlurb blurb={dummyBlurb} />
-         <div className="mt-10">
-            <h1 className="font-josefin text-3xl">Top Blurbs:</h1>
-            <div className="grid grid-cols-3 gap-10 w-full mt-2">
+         <div className="mt-16">
+            <div className="flex items-center justify-between">
+               <h1 className="font-josefin text-3xl">Top Blurbs</h1>
+               <Link
+                  href={paths.explorePage()}
+                  className="hover:border-b border-b-primary"
+               >
+                  <span className="flex gap-2 items-center text-primary">
+                     See all <ArrowRight size={20} />
+                  </span>
+               </Link>
+            </div>
+            <div className="grid grid-cols-3 gap-10 w-full mt-4">
                <DisplayBlurb blurb={dummyBlurb} />
                <DisplayBlurb blurb={dummyBlurb} />
                <DisplayBlurb blurb={dummyBlurb} />
+            </div>
+         </div>
+         <div className="mt-16">
+            <h1 className="font-josefin text-3xl">Community Highlights</h1>
+            <div className="grid grid-cols-2 gap-5 mt-4">
+               <DataPoint
+                  colour="blue"
+                  dataValue="12"
+                  size="xlarge"
+                  position="top"
+               >
+                  Total Number of Users
+               </DataPoint>
+               <div className="grid-rows-2 gap-5 grid">
+                  <DataPoint
+                     colour="green"
+                     position="bottom"
+                     dataValue="540"
+                     size="medium"
+                  >
+                     Unique Blurbs Written
+                  </DataPoint>
+                  <DataPoint
+                     colour="yellow"
+                     position="left"
+                     dataValue="Jerry Shum"
+                     size="small"
+                  >
+                     Top Contributor This Week
+                  </DataPoint>
+               </div>
             </div>
          </div>
       </div>
