d158b4a1a3945a2ae7987539064195050a43396b
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-17 11:27:18 -0500

    importing the md editor into blurb create form

--- src/components/blurbs/CreateBlurbForm.tsx
+++ src/components/blurbs/CreateBlurbForm.tsx
@@ -4,8 +4,20 @@ import { useForm } from "react-hook-form";
 import { Input } from "../UI/input";
 import { Label } from "../UI/label";
 import { Button } from "../UI/button";
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+import * as commands from "@uiw/react-md-editor/commands";
+
+const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
 
 export default function CreateBlurbForm() {
+  const [MDvalue, setMDValue] = useState(
+    "**Use markdown for the main content of your blurb!**",
+  );
+
   const {
     register,
     handleSubmit,
@@ -30,7 +42,7 @@ export default function CreateBlurbForm() {
         Short Description of Blurb Content
       </Label>
       <textarea
-        {...register("title")}
+        {...register("description")}
         required
         placeholder="Enter a short description for your blurb..."
         className="col-span-4 h-full rounded-xl border border-muted-foreground p-2 text-muted-foreground lg:min-h-32"
@@ -45,7 +57,7 @@ export default function CreateBlurbForm() {
       </div>
       <div className="col-span-4 flex justify-center">
         <input
-          {...register("title")}
+          {...register("image")}
           type="file"
           required
           className="text-muted-foreground"
@@ -53,7 +65,41 @@ export default function CreateBlurbForm() {
         />
       </div>
 
-      <Button disabled={isSubmitting} className="col-span-6">
+      <div className="col-span-6 flex flex-col justify-start gap-4">
+        <div>
+          <Label className="xl:text-lg">Blurb&apos;s main content:</Label>
+          <h4 className="text-xs text-muted-foreground">
+            Using markdown, you can write the main content for your blurb!
+          </h4>
+        </div>
+        <MDEditor
+          value={MDvalue}
+          onChange={(val) => setMDValue(val || "")}
+          className="lg:min-h-80"
+        />
+      </div>
+
+      <input
+        {...register("mainContent")}
+        type="text"
+        hidden
+        value={MDvalue}
+        placeholder="Enter the title of your blurb..."
+      />
+
+      <div className="col-span-6 flex flex-col justify-start gap-4">
+        <Label className="xl:text-lg" htmlFor="markdownFile">
+          Or upload a markdown file:
+        </Label>
+        <input
+          {...register("markdownFile")}
+          type="file"
+          accept=".md"
+          className="text-muted-foreground"
+        />
+      </div>
+
+      <Button disabled={isSubmitting || isLoading} className="col-span-6">
         Submit
       </Button>
     </form>
