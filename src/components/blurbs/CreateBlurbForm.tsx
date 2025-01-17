685e70d6e084b1e1ebc1c808f65284a4aa6df892
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-17 09:14:18 -0500

    making the general structure of the createblurbform using RHF

---
+++ src/components/blurbs/CreateBlurbForm.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { Input } from "../UI/input";
+import { Label } from "../UI/label";
+import { Button } from "../UI/button";
+
+export default function CreateBlurbForm() {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    reset,
+    formState: { error, isSubmitting, isLoading },
+  } = useForm();
+
+  return (
+    <form className="my-10 grid grid-cols-6 items-start gap-10 gap-y-10 py-4 sm:px-10 md:px-20 xl:px-40 2xl:px-60">
+      <Label className="col-span-2 xl:text-lg" htmlFor="title">
+        Title of your Blurb
+      </Label>
+      <input
+        {...register("title")}
+        type="text"
+        required
+        placeholder="Enter the title of your blurb..."
+        className="col-span-4 justify-end rounded-xl border border-muted-foreground p-2 text-muted-foreground"
+      />
+      <Label className="col-span-2 xl:text-lg" htmlFor="title">
+        Short Description of Blurb Content
+      </Label>
+      <textarea
+        {...register("title")}
+        required
+        placeholder="Enter a short description for your blurb..."
+        className="col-span-4 h-full rounded-xl border border-muted-foreground p-2 text-muted-foreground lg:min-h-32"
+      />
+      <div className="col-span-2 flex w-full flex-col gap-2">
+        <Label className="col-span-2 xl:text-lg" htmlFor="title">
+          Image to Display with Blurb
+        </Label>
+        <h4 className="text-xs text-muted-foreground">
+          Image should have a resolution of 1920x1080 and be in .jpg or .png
+        </h4>
+      </div>
+      <div className="col-span-4 flex justify-center">
+        <input
+          {...register("title")}
+          type="file"
+          required
+          className="text-muted-foreground"
+          accept=".jpg, .png"
+        />
+      </div>
+
+      <Button disabled={isSubmitting} className="col-span-6">
+        Submit
+      </Button>
+    </form>
+  );
+}
