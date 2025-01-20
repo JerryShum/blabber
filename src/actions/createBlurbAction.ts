cd1685d557734e29719028c7f0f3e217e2d45e7d
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-20 15:59:27 -0500

    adding the blurb creation logic -> also adding error catching

--- src/actions/createBlurbAction.ts
+++ src/actions/createBlurbAction.ts
@@ -1,18 +1,83 @@
 "use server";
 
 import { createBlurbSchema } from "@/_schemas/createBlurbSchema";
-import { FormState } from "react-hook-form";
+import { auth } from "@/auth";
+import { prisma } from "@/db";
 
-export async function createBlurbAction(data: FormData): Promise<FormState> {
+type FormState = {
+  status: "idle" | "pending" | "error" | "success";
+  errors: {
+    title?: string[];
+    description?: string[];
+    image?: string[];
+    mainContent?: string[];
+    _form?: string[];
+  };
+};
+
+export async function createBlurbAction(
+  prevState: FormState,
+  data: FormData,
+): Promise<FormState> {
   const actualData = Object.fromEntries(data.entries());
-  console.log("sigma:", actualData);
-  console.log(actualData.image);
   const parseResult = createBlurbSchema.safeParse(actualData);
-  console.log(parseResult.error);
 
+  //! If form validation fails, return the errors received from the schema
   if (!parseResult.success) {
+    console.log(parseResult.error);
     return {
-      errors: "Invalid data",
+      status: "error",
+      errors: parseResult.error.flatten().fieldErrors,
+    };
+  } else {
+    //! No form validation errors, so proceed with DB
+
+    try {
+      //! Check if user is logged in -> get the id
+      const session = await auth();
+      if (!session || !session.user) {
+        return {
+          status: "error",
+          errors: { _form: ["You must be logged in to create a blurb"] },
+        };
+      }
+
+      //! Calculate the read time of the blurb
+      const readTime = Math.ceil(
+        actualData.mainContent.toString().trim().length / 1000,
+      ).toString();
+
+      const newBlurb = await prisma.blurb.create({
+        data: {
+          title: actualData.title.toString(),
+          description: actualData.description.toString(),
+          image: actualData.image.toString(),
+          content: actualData.mainContent.toString(),
+          authorId: session?.user?.id ?? "",
+          estimatedRead: readTime,
+        },
+      });
+
+      console.log("Created a new blurb:", newBlurb);
+    } catch (error: unknown) {
+      //! If we know what the error is:
+      if (error instanceof Error) {
+        return {
+          status: "error",
+          errors: { _form: [error.message] },
+        };
+      } else {
+        //! If we don't know what the error is:
+        return {
+          status: "error",
+          errors: { _form: ["An unknown error occurred"] },
+        };
+      }
+    }
+
+    return {
+      status: "success",
+      errors: {},
     };
   }
 }
