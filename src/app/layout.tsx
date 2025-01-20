460f4b2c1e8874c294ce478eea841c7e7c048a11
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-20 16:10:15 -0500

    adding toaster to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,46 @@
-import type { Metadata } from 'next';
-import { Noto_Sans } from 'next/font/google';
-import { Josefin_Sans } from 'next/font/google';
-import './globals.css';
-import Navbar from '@/components/nav/Navbar';
-import Providers from './Providers';
-import Footer from '@/components/footer/Footer';
+import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/nav/Navbar";
+import Providers from "./Providers";
+import Footer from "@/components/footer/Footer";
+import { Toaster } from "react-hot-toast";
 
 const notoSans = Noto_Sans({
-   subsets: ['latin'],
-   variable: '--font-noto-sans',
+  subsets: ["latin"],
+  variable: "--font-noto-sans",
 });
 
 const josefinSans = Josefin_Sans({
-   subsets: ['latin'],
-   variable: '--font-josefin-sans',
+  subsets: ["latin"],
+  variable: "--font-josefin-sans",
 });
 
 export const metadata: Metadata = {
-   title: 'Blabber',
-   description: 'Minimalistic and simple blog platform.',
+  title: "Blabber",
+  description: "Minimalistic and simple blog platform.",
 };
 
 export default function RootLayout({
-   children,
+  children,
 }: Readonly<{
-   children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-   return (
-      <html lang="en">
-         <body
-            className={`${josefinSans.variable} ${notoSans.variable} antialiased `}
-         >
-            <Providers>
-               <div className="sm:px-10 md:px-20 xl:px-30 2xl:px-60 relative">
-                  <Navbar />
-                  <main>{children}</main>
-                  <Footer />
-               </div>
-            </Providers>
-         </body>
-      </html>
-   );
+  return (
+    <html lang="en">
+      <body
+        className={`${josefinSans.variable} ${notoSans.variable} antialiased`}
+      >
+        <Providers>
+          <div className="xl:px-30 relative sm:px-10 md:px-20 2xl:px-60">
+            <Navbar />
+            <Toaster />
+            <main>{children}</main>
+            <Footer />
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
