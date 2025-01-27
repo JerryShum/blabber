4b67ba4064609ed83d91e7ebc3162f2522bbfa83
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-27 17:21:47 -0500

    defining the "view" blurb page and route

--- src/app/blurbs/[blurbID]/page.tsx
+++ src/app/blurbs/[blurbID]/page.tsx
@@ -1,3 +1,18 @@
-export default function page() {
-   return <div>Display blurb page</div>;
+import { getBlurbsID } from "@/lib/queries/getBlurbs";
+
+interface Params {
+  blurbID: string;
+}
+
+export default async function Page({ params }: { params: Params }) {
+  const { blurbID } = await params;
+  const blurb = await getBlurbsID(blurbID);
+  console.log(blurb);
+
+  return (
+    <div>
+      <h1>Display Blurb Page</h1>
+      <p>Blurb ID: {blurbID}</p>
+    </div>
+  );
 }
