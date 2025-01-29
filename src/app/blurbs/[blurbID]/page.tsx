d6bfd53dbb2d7b579f8d1b680c5d3dd5b85215fd
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-29 12:07:10 -0500

    defining structure of the display blurb page

--- src/app/blurbs/[blurbID]/page.tsx
+++ src/app/blurbs/[blurbID]/page.tsx
@@ -1,4 +1,6 @@
+import { AspectRatio } from "@/components/UI/aspect-ratio";
 import { getBlurbsID } from "@/lib/queries/getBlurbs";
+import Image from "next/image";
 
 interface Params {
   blurbID: string;
@@ -9,10 +11,48 @@ export default async function Page({ params }: { params: Params }) {
   const blurb = await getBlurbsID(blurbID);
   console.log(blurb);
 
+  const sampleBlurb = {
+    id: 27,
+    description:
+      "short descriptionTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest",
+    content:
+      "**Use markdown for the main content of your blurb!**testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest",
+    authorId: "cm5h1fvam0000ui4cnslmlao7",
+    createdAt: new Date("2025-01-27T22:28:56.779Z"),
+    image:
+      "https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg",
+    title: "TestTestTestTestTest",
+    estimatedRead: "1",
+    author: { name: "Jerry Shum", email: "jerryshum2636@gmail.com" },
+  };
+
   return (
-    <div>
-      <h1>Display Blurb Page</h1>
-      <p>Blurb ID: {blurbID}</p>
+    <div className="mt-20">
+      <div className="space-y-2">
+        <h1 className="text-5xl font-bold">{blurb.title}</h1>
+        <h2 className="text-xl text-muted-foreground">{blurb.description}</h2>
+      </div>
+
+      <div className="relative">
+        <AspectRatio className="mt-10" ratio={16 / 9}>
+          <Image
+            src={blurb?.image}
+            className="object-cover object-center"
+            quality={100}
+            alt="image of interior design"
+            fill
+          />
+        </AspectRatio>
+
+        <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+          <p className="text-white">{blurb.author.name}</p>
+          <p>{new Date(blurb.createdAt).toLocaleDateString()}</p>
+        </div>
+      </div>
+
+      <div>
+        <p>{blurb.content}</p>
+      </div>
     </div>
   );
 }
