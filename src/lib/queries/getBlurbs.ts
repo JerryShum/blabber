29958528a7632283d9e76aef3335e6e91424ee9c
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-31 11:15:45 -0500

    update query to inlcude incrementing the view

--- src/lib/queries/getBlurbs.ts
+++ src/lib/queries/getBlurbs.ts
@@ -2,10 +2,25 @@ import { prisma } from "@/db";
 import { Blurb } from "@prisma/client";
 
 export async function getBlurbsID(blurbID: string): Promise<Blurb | null> {
-  return await prisma.blurb.findUnique({
-    where: { id: parseInt(blurbID) },
-    include: {
-      author: { select: { name: true, email: true } },
-    },
+  const blurb = await prisma.$transaction(async (prisma) => {
+    // Get the blurb
+    const blurb = await prisma.blurb.findUnique({
+      where: { id: parseInt(blurbID) },
+      include: {
+        author: { select: { name: true, email: true } },
+      },
+    });
+
+    if (!blurb) return null;
+
+    // Increment the view count
+    await prisma.blurb.update({
+      where: { id: parseInt(blurbID) },
+      data: { views: { increment: 1 } },
+    });
+
+    return blurb;
   });
+
+  return blurb;
 }
