f57b959dd961777994b7f6de5a6d131697774616
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-01-31 11:42:40 -0500

    fetching the top4 blurbs -> displaying in order of views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,23 +7,14 @@ import Link from "next/link";
 import paths from "@/paths";
 import DataPoint from "@/components/common/DataPoint";
 import { stringify } from "querystring";
+import { getTop4Blurbs } from "@/lib/queries/getBlurbs";
 
-const dummyBlurb: Blurb = {
-  id: 1,
-  title: "Welcome to Blabber",
-  description: "This is a dummy blurb for the hero section.",
-  content:
-    " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  image: "/DummyHeroBlurb.jpg",
-  authorId: "1",
-  createdAt: new Date(),
-  estimatedRead: "10",
-};
+const blurbs = await getTop4Blurbs();
 
 export default async function Home() {
   return (
     <div className="">
-      <HeroBlurb blurb={dummyBlurb} />
+      <HeroBlurb blurb={blurbs[0]} />
       <div className="mt-16">
         <div className="flex items-center justify-between">
           <h1 className="font-josefin text-3xl">Top Blurbs</h1>
@@ -37,9 +28,9 @@ export default async function Home() {
           </Link>
         </div>
         <div className="mt-4 grid w-full grid-cols-3 gap-10">
-          <DisplayBlurb blurb={dummyBlurb} />
-          <DisplayBlurb blurb={dummyBlurb} />
-          <DisplayBlurb blurb={dummyBlurb} />
+          {blurbs.slice(1).map((blurb: Blurb) => (
+            <DisplayBlurb blurb={blurb} key={blurb.id} />
+          ))}
         </div>
       </div>
       <div className="mt-16">
