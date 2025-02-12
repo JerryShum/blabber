ce08042297c542b4ecbb056a7074769e37cb6be5
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-02-12 09:11:38 -0500

    new queries for explore page

--- src/lib/queries/getBlurbs.ts
+++ src/lib/queries/getBlurbs.ts
@@ -36,3 +36,39 @@ export async function getTop4Blurbs(): Promise<Blurb[]> {
 
   return blurbs;
 }
+
+//! Explore Page:
+export async function getNewestBlurbs(): Promise<Blurb[]> {
+  const blurbs = await prisma.blurb.findMany({
+    include: {
+      author: true,
+    },
+    orderBy: { createdAt: "desc" },
+    take: 6,
+  });
+
+  return blurbs;
+}
+
+export async function getTrendingBlurbs(): Promise<Blurb[]> {
+  const now = new Date();
+  const yesterday = new Date(now);
+  yesterday.setDate(now.getDate() - 1); // Get the date 24 hours ago
+
+  const blurbs = await prisma.blurb.findMany({
+    where: {
+      createdAt: {
+        gte: yesterday, // Filter for blurbs created in the last 24 hours
+      },
+    },
+    include: {
+      author: true,
+    },
+    orderBy: {
+      views: "desc", // Order by views in descending order
+    },
+    take: 6, // Adjust the number of blurbs you want to fetch
+  });
+
+  return blurbs;
+}
