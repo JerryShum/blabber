c65f0f7c579f34e227a6bc934bdc7a23ea060e41
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2025-02-15 10:01:04 -0500

    making the navLinks responsive

--- src/components/nav/NavLinks.tsx
+++ src/components/nav/NavLinks.tsx
@@ -1,44 +1,44 @@
-import paths from '@/paths';
-import Link from 'next/link';
-import { buttonVariants } from '../UI/button';
+import paths from "@/paths";
+import Link from "next/link";
+import { buttonVariants } from "../UI/button";
 
 export default function NavLinks() {
-   return (
-      <nav>
-         <ul className="flex items-center gap-1 ">
-            <li>
-               <Link
-                  className={buttonVariants({ variant: 'link' })}
-                  href={paths.home()}
-               >
-                  <span className="text-base font-josefin">Home</span>
-               </Link>
-            </li>
-            <li>
-               <Link
-                  className={buttonVariants({ variant: 'link' })}
-                  href={paths.explorePage()}
-               >
-                  <span className="text-base font-josefin">Explore</span>
-               </Link>
-            </li>
-            <li>
-               <Link
-                  className={buttonVariants({ variant: 'link' })}
-                  href={paths.aboutPage()}
-               >
-                  <span className="text-base font-josefin">About Us</span>
-               </Link>
-            </li>
-            <li>
-               <Link
-                  className={buttonVariants({ variant: 'link' })}
-                  href={paths.CreatePage()}
-               >
-                  <span className="text-base font-josefin">Create Blurb</span>
-               </Link>
-            </li>
-         </ul>
-      </nav>
-   );
+  return (
+    <nav>
+      <ul className="flex items-center gap-1">
+        <li>
+          <Link
+            className={buttonVariants({ variant: "link" })}
+            href={paths.home()}
+          >
+            <span className="font-josefin text-base">Home</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={buttonVariants({ variant: "link" })}
+            href={paths.explorePage()}
+          >
+            <span className="font-josefin text-base">Explore</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={buttonVariants({ variant: "link" })}
+            href={paths.aboutPage()}
+          >
+            <span className="font-josefin text-base">About Us</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={buttonVariants({ variant: "link" })}
+            href={paths.CreatePage()}
+          >
+            <span className="font-josefin text-base">Create Blurb</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
